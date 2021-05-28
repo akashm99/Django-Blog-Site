b6a8774cfdf40461a524d3fe22f2a9736108cd16
akashm99 <akashbm08@gmail.com>
2021-05-28 21:03:58 +0530

    initial commit

---
+++ django_project/blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'AkashBM',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'CoreyMs',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
