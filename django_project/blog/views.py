3323798af0bc00e5ab827faef7fb38d5b1108bf8
akashm99 <akashbm08@gmail.com>
2021-05-29 12:31:12 +0530

    database models Post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'AkashBM',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'CoreyMs',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
